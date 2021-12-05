e54ebe518e298f0df769cc037755f01345ced026
Asanka <asanka@my.yorku.ca>
2021-12-05 18:27:40 -0500

    1st working commit

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,27 @@
 package main;
 
+import controller.Conversion;
+import model.ValueToConvert;
+import view.CentimeterConversionArea;
+import view.ConversionAreaObserver;
+import view.FeetConversionArea;
+import view.MeterConversionArea;
+
+
 public class Main {
+	
+	public static void main(String[] args) {
+		ValueToConvert recevier = new ValueToConvert();
+		
+		ConversionAreaObserver fArea = new FeetConversionArea(recevier);
+		ConversionAreaObserver mArea = new MeterConversionArea(recevier);
+		
+		recevier.register(fArea);
+		recevier.register(mArea);
+		
+		Conversion command = new Conversion(recevier);
+		CentimeterConversionArea cArea = new CentimeterConversionArea(command, 5);
+		cArea.press();
+	}
 
 }
